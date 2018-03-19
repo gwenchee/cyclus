3c843d4c37188f3e2777566142bf6bdf6e8261f7
Gwendolyn Chee <gwenchee@Gwendolyns-MacBook-Pro.local>
2018-03-19 17:35:24 -0400

    added cycexception cc file these r taken from 2013 version of cyclus from sjenitzer fork

---
+++ src/CycException.cc
@@ -0,0 +1,30 @@
+// CycException.cpp
+// Implements the CycException class
+#include <sstream>
+
+#include "CycException.h"
+
+using namespace std;
+
+string itoa(int i)    { stringstream out; out << i; return out.str(); };
+string dtoa(double d) { stringstream out; out << d; return out.str(); };
+
+string CycException::prepend_ = "cyclus exception";
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+CycException::CycException() {
+	myMessage_ = prepend_;
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+CycException::CycException(std::string msg) {
+	myMessage_ = prepend_ + ": " + msg;
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const char* CycException::what() const throw() {
+	//const char* toRet = myMessage_;
+	//	return toRet;
+	return myMessage_.c_str();
+}
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+CycException::~CycException() throw()  { }
