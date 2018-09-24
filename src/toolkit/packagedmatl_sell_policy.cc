b5ddc6caad4f9e6658e6ca9e07c76cd00a58be9a
gwenchee <gwendolynchee95@gmail.com>
2018-09-24 10:33:48 -0500

    trying

---
+++ src/toolkit/packagedmatl_sell_policy.cc
@@ -0,0 +1,188 @@
+#include "packagedmatl_sell_policy.h"
+
+#include "error.h"
+
+#define LG(X) LOG(LEV_##X, "selpol")
+#define LGH(X)                                                    \
+  LOG(LEV_##X, "selpol") << "policy " << name_ << " (agent "      \
+                         << Trader::manager()->prototype() << "-" \
+                         << Trader::manager()->id() << "): "
+
+namespace cyclus {
+namespace toolkit {
+
+PackagedMatlSellPolicy::PackagedMatlSellPolicy() :
+    Trader(NULL),
+    name_(""),
+    quantize_(-1),
+    throughput_(std::numeric_limits<double>::max()),
+    ignore_comp_(false) {
+  Warn<EXPERIMENTAL_WARNING>(
+      "PackagedMatlSellPolicy is experimental and its API may be subject to change");
+}
+
+PackagedMatlSellPolicy::~PackagedMatlSellPolicy() {
+  if (manager() != NULL)
+    manager()->context()->UnregisterTrader(this);
+}
+
+void PackagedMatlSellPolicy::set_quantize(double x) {
+  assert(x != 0);
+  quantize_ = x;
+}
+
+void PackagedMatlSellPolicy::set_throughput(double x) {
+  assert(x >= 0);
+  throughput_ = x;
+}
+
+void PackagedMatlSellPolicy::set_ignore_comp(bool x) {
+  ignore_comp_ = x;
+}
+
+PackagedMatlSellPolicy& PackagedMatlSellPolicy::Init(Agent* manager, ResBuf<PackagedMaterial>* buf,
+                                     std::string name) {
+  Trader::manager_ = manager;
+  buf_ = buf;
+  name_ = name;
+  return *this;
+}
+
+PackagedMatlSellPolicy& PackagedMatlSellPolicy::Init(Agent* manager, ResBuf<PackagedMaterial>* buf,
+                                     std::string name, double throughput) {
+  Trader::manager_ = manager;
+  buf_ = buf;
+  name_ = name;
+  set_throughput(throughput);
+  return *this;
+}
+
+PackagedMatlSellPolicy& PackagedMatlSellPolicy::Init(Agent* manager, ResBuf<PackagedMaterial>* buf,
+                                     std::string name, bool ignore_comp) {
+  Trader::manager_ = manager;
+  buf_ = buf;
+  name_ = name;
+  set_ignore_comp(ignore_comp);
+  return *this;
+}
+
+PackagedMatlSellPolicy& PackagedMatlSellPolicy::Init(Agent* manager, ResBuf<PackagedMaterial>* buf,
+                                     std::string name, double throughput,
+                                     bool ignore_comp) {
+  Trader::manager_ = manager;
+  buf_ = buf;
+  name_ = name;
+  set_throughput(throughput);
+  set_ignore_comp(ignore_comp);
+  return *this;
+}
+
+PackagedMatlSellPolicy& PackagedMatlSellPolicy::Init(Agent* manager, ResBuf<PackagedMaterial>* buf,
+                                     std::string name, double throughput,
+                                     bool ignore_comp, double quantize) {
+  Trader::manager_ = manager;
+  buf_ = buf;
+  name_ = name;
+  set_quantize(quantize);
+  set_throughput(throughput);
+  set_ignore_comp(ignore_comp);
+  return *this;
+}
+
+PackagedMatlSellPolicy& PackagedMatlSellPolicy::Set(std::string commod) {
+  commods_.insert(commod);
+  return *this;
+}
+
+void PackagedMatlSellPolicy::Start() {
+  if (manager() == NULL) {
+    std::stringstream ss;
+    ss << "No manager set on Sell Policy " << name_;
+    throw ValueError(ss.str());
+  }
+  manager()->context()->RegisterTrader(this);
+}
+
+void PackagedMatlSellPolicy::Stop() {
+  if (manager() == NULL) {
+    std::stringstream ss;
+    ss << "No manager set on Sell Policy " << name_;
+    throw ValueError(ss.str());
+  }
+  manager()->context()->UnregisterTrader(this);
+}
+
+
+double PackagedMatlSellPolicy::Limit() const {
+  double bcap = buf_->quantity();
+  double limit = Excl() ?                                               \
+                 quantize_ * static_cast<int>(std::floor(bcap / quantize_)) : bcap;
+  return std::min(throughput_, limit);
+}
+
+std::set<BidPortfolio<PackagedMaterial>::Ptr> PackagedMatlSellPolicy::GetPackagedMatlBids(
+    CommodMap<PackagedMaterial>::type& commod_requests) {
+  std::set<BidPortfolio<PackagedMaterial>::Ptr> ports;
+  if (buf_->empty() || buf_->quantity() < eps())
+    return ports;
+
+  BidPortfolio<PackagedMaterial>::Ptr port(new BidPortfolio<PackagedMaterial>());
+
+  double limit = Limit();
+  CapacityConstraint<PackagedMaterial> cc(limit);
+  port->AddConstraint(cc);
+  ports.insert(port);
+  LGH(INFO3) << "bidding out " << limit << " kg";
+  
+  bool excl = Excl();
+  std::string commod;
+  Request<PackagedMaterial>* req;
+  PackagedMaterial::Ptr pm, offer;
+  double qty;
+  int nbids;
+  std::set<std::string>::iterator sit;
+  std::vector<Request<PackagedMaterial>*>::const_iterator rit;
+  for (sit = commods_.begin(); sit != commods_.end(); ++sit) {
+    commod = *sit;
+    if (commod_requests.count(commod) < 1)
+      continue;
+
+    const std::vector<Request<PackagedMaterial>*>& requests =
+        commod_requests.at(commod);
+    for (rit = requests.begin(); rit != requests.end(); ++rit) {
+      req = *rit;
+      qty = std::min(req->target()->quantity(), limit);
+      nbids = excl ? static_cast<int>(std::floor(qty / quantize_)) : 1;
+      qty = excl ? quantize_ : qty;
+      for (int i = 0; i < nbids; i++) {
+        pm = buf_->Pop();
+        buf_->Push(pm);
+        offer = ignore_comp_ ? \
+                PackagedMaterial::CreateUntracked(qty, req->target()->package()) : \
+                PackagedMaterial::CreateUntracked(qty, pm->package());
+        port->AddBid(req, offer, this, excl);
+        LG(INFO3) << "  - bid " << qty << " kg on a request for " << commod;
+      }
+    }
+  }
+  return ports;
+}
+
+void PackagedMatlSellPolicy::GetPackagedMatlTrades(
+    const std::vector<Trade<PackagedMaterial> >& trades,
+    std::vector<std::pair<Trade<PackagedMaterial>, PackagedMaterial::Ptr> >& responses) {
+  Composition::Ptr c;
+  std::vector<Trade<PackagedMaterial> >::const_iterator it;
+  for (it = trades.begin(); it != trades.end(); ++it) {
+    double qty = it->amt;
+    LGH(INFO3) << " sending " << qty << " kg of " << it->request->commodity();
+    Material::Ptr mat = buf_->Pop(qty, cyclus::eps_rsrc());
+    if (ignore_comp_)
+      mat->Transmute(it->request->target()->package());
+    responses.push_back(std::make_pair(*it, mat));
+  }
+}
+
+}  // namespace toolkit
+}  // namespace cyclus
+
---
+++ src/toolkit/packagedmatl_sell_policy.h
@@ -0,0 +1,141 @@
+#ifndef CYCLUS_SRC_TOOLKIT_PACKAGED_MATL_SELL_POLICY_H_
+#define CYCLUS_SRC_TOOLKIT_PACKAGED_MATL_SELL_POLICY_H_
+
+#include <string>
+
+#include "composition.h"
+#include "material.h"
+#include "res_buf.h"
+#include "trader.h"
+#include "packagedmaterial.h"
+
+namespace cyclus {
+namespace toolkit {
+
+/// MatlSellPolicy performs semi-automatic inventory management of a material
+/// buffer by making offers and trading away materials in an attempt to empty
+/// the buffer's inventory every time step.
+///
+/// For simple behavior, policies virtually eliminate the need to write any code
+/// for resource exchange. Just assign a few policies to work with a few buffers
+/// and focus on writing the physics and other behvavior of your agent.  Typical
+/// usage goes something like this:
+///
+/// @code
+/// class YourAgent : public Facility {
+///  public:
+///   ...
+///
+///   void EnterNotify() {
+///     Facility::EnterNotify(); // always do this first
+///
+///     policy_.Init(this, &outbuf_, "outbuf-label").Set(outcommod).Start();
+///   }
+///   ...
+///
+///  private:
+///   MatlSellPolicy policy_;
+///   ResBuf<Material> outbuf_;
+///    ...
+/// }
+/// @endcode
+///
+/// The policy needs to be initialized with its owning agent and the material
+/// buffer that is is managing. It also needs to be activated by calling the
+/// Start function for it to begin participation in resource exchange.  And
+/// don't forget to add some commodities to offer on by calling Set.  All policy
+/// configuration should usually occur in the agent's EnterNotify member
+/// function.
+///
+/// @warn When a policy's managing agent is deallocated, you MUST either
+/// call the policy's Stop function or delete the policy. Otherwise SEGFAULT.
+class PackagedMatlSellPolicy : public Trader {
+ public:
+  /// Creates an uninitialized policy.  The Init function MUST be called before
+  /// anything else is done with the policy.
+  PackagedMatlSellPolicy();
+
+  virtual ~PackagedMatlSellPolicy();
+
+  /// Configures the policy to keep buf empty every time step by offering away
+  /// as much material as possible.  If quantize is greater than zero, the
+  /// policy will make exclusive, integral quantize kg offers.  Otherwise, a
+  /// single offer will be sent each time step to empty the buffer's entire
+  /// inventory.
+  /// @param manager the agent
+  /// @param buf the resource buffer
+  /// @param name a unique name identifying this policy
+  /// @param throughput a maximum throughput per time step for material leaving
+  /// buf
+  /// @ignore_comp ignore the composition of material in buf. When making bids,
+  /// requested material compositions will be used. When making trades,
+  /// material in the buffer will have its composition transmuted to match the
+  /// requested material.
+  /// @param quantize If quantize is greater than zero, the policy will make
+  /// exclusive, integral quantize kg bids. Otherwise, single bids will
+  /// be sent matching the requested quantity.
+  /// @{
+  PackagedMatlSellPolicy& Init(Agent* manager, ResBuf<PackagedMaterial>* buf, std::string name);
+  PackagedMatlSellPolicy& Init(Agent* manager, ResBuf<PackagedMaterial>* buf, std::string name,
+                       double throughput);
+  PackagedMatlSellPolicy& Init(Agent* manager, ResBuf<PackagedMaterial>* buf, std::string name,
+                       bool ignore_comp);
+  PackagedMatlSellPolicy& Init(Agent* manager, ResBuf<PackagedMaterial>* buf, std::string name,
+                       double throughput, bool ignore_comp);
+  PackagedMatlSellPolicy& Init(Agent* manager, ResBuf<PackagedMaterial>* buf, std::string name,
+                       double throughput, bool ignore_comp, 
+                       double quantize);
+  /// @}
+  
+  /// Instructs the policy to empty its buffer with offers on the given
+  /// commodity.  This must be called at least once or the policy will do
+  /// nothing.  The policy can offer on an arbitrary number of commodities by
+  /// calling Set multiple times.
+  PackagedMatlSellPolicy& Set(std::string commod);
+
+  /// Registers this policy as a trader in the current simulation.  This
+  /// function must be called for the policy to begin participating in resource
+  /// exchange. Init MUST be called prior to calling this function.  Start is
+  /// idempotent.
+  void Start();
+
+  /// Unregisters this policy as a trader in the current simulation. This
+  /// function need only be called if a policy is to be stopped *during* a
+  /// simulation (it is not required to be called explicitly at the end). Stop
+  /// is idempotent.
+  void Stop();
+
+  /// the current (total) limit on transactions, i.e., the quantity of resources
+  /// that can be transacted in a time step
+  double Limit() const;
+
+  /// whether trades will be denoted as exclusive or not
+  inline bool Excl() const { return quantize_ > 0; }
+  
+  /// Trader Methods
+  /// @{
+  virtual std::set<BidPortfolio<PackagedMaterial>::Ptr> GetPackagedMatlBids(
+      CommodMap<PackagedMaterial>::type& commod_requests);
+  virtual void GetPackagedMatlTrades(
+      const std::vector<Trade<PackagedMaterial> >& trades,
+      std::vector<std::pair<Trade<PackagedMaterial>, PackagedMaterial::Ptr> >& responses);
+  /// }@
+  
+ private:
+  void set_quantize(double x);
+  void set_throughput(double x);
+  void set_ignore_comp(bool x);
+  
+  ResBuf<PackagedMaterial>* buf_;
+  std::set<std::string> commods_;
+  double quantize_;
+  double throughput_;
+  std::string name_;
+  bool ignore_comp_;
+};
+
+}  // namespace toolkit
+}  // namespace cyclus
+
+#endif
+
