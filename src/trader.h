9ecccbf61c604f28b3b78106f9ae1f4c3dc4869d
gwenchee <gwendolynchee95@gmail.com>
2018-10-29 17:47:19 -0500

    added packagedmaterial

--- src/trader.h
+++ src/trader.h
@@ -10,6 +10,7 @@
 #include "material.h"
 #include "request_portfolio.h"
 #include "trade.h"
+#include "packagedmaterial.h"
 
 namespace cyclus {
 
@@ -41,6 +42,12 @@ class Trader {
     return std::set<RequestPortfolio<Product>::Ptr>();
   }
 
+  /// @brief default implementation for packagedmaterial requests
+  virtual std::set<RequestPortfolio<PackagedMaterial>::Ptr>
+      GetPackagedMatlRequests() {
+    return std::set<RequestPortfolio<PackagedMaterial>::Ptr>();
+  }
+
   /// @brief default implementation for material requests
   virtual std::set<BidPortfolio<Material>::Ptr>
       GetMatlBids(CommodMap<Material>::type& commod_requests) {
@@ -53,11 +60,21 @@ class Trader {
     return std::set<BidPortfolio<Product>::Ptr>();
   }
 
+  /// @brief default implementation for packagedmaterial requests
+  virtual std::set<BidPortfolio<PackagedMaterial>::Ptr>
+      GetPackagedMatlBids(CommodMap<PackagedMaterial>::type& commod_requests) {
+    return std::set<BidPortfolio<PackagedMaterial>::Ptr>();
+  }
+
   /// default implementation for material preferences.
   virtual void AdjustMatlPrefs(PrefMap<Material>::type& prefs) {}
 
-  /// default implementation for material preferences.
+  /// default implementation for product preferences.
   virtual void AdjustProductPrefs(PrefMap<Product>::type& prefs) {}
+
+  /// default implementation for packagedmaterial preferences.
+  virtual void AdjustPackagedMatlPrefs(PrefMap<PackagedMaterial>::type& prefs) {}
+
 
   /// @brief default implementation for responding to material trades
   /// @param trades all trades in which this trader is the supplier
@@ -74,6 +91,14 @@ class Trader {
       std::vector<std::pair<Trade<Product>,
       Product::Ptr> >& responses) {}
 
+  /// @brief default implementation for responding to packagedmaterial trades
+  /// @param trades all trades in which this trader is the supplier
+  /// @param responses a container to populate with responses to each trade
+  virtual void GetPackagedMatlTrades(
+      const std::vector< Trade<PackagedMaterial> >& trades,
+      std::vector<std::pair<Trade<PackagedMaterial>,
+      PackagedMaterial::Ptr> >& responses) {}
+
   /// @brief default implementation for material trade acceptance
   virtual void AcceptMatlTrades(
       const std::vector<std::pair<Trade<Material>,
@@ -83,6 +108,11 @@ class Trader {
   virtual void AcceptProductTrades(
       const std::vector<std::pair<Trade<Product>,
       Product::Ptr> >& responses) {}
+
+  /// @brief default implementation for packagedmaterial trade acceptance
+  virtual void AcceptPackagedMatlTrades(
+      const std::vector<std::pair<Trade<PackagedMaterial>,
+      PackagedMaterial::Ptr> >& responses) {}
 
  protected:
   Agent* manager_;
@@ -101,6 +131,14 @@ class Trader {
       GetProductBids(const CommodMap<Product>::type& commod_requests) {
     return std::set<BidPortfolio<Product>::Ptr>();
   }
+
+  /// @warning this function is hidden to prevent an invalid signature that can
+  /// raise difficult to find bugs
+  virtual std::set<BidPortfolio<PackagedMaterial>::Ptr>
+      GetPackagedMaterialBids(const CommodMap<PackagedMaterial>::type& commod_requests) {
+    return std::set<BidPortfolio<PackagedMaterial>::Ptr>();
+  }
+
 };
 
 }  // namespace cyclus
